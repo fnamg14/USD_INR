5e50c05a8046758e6caaf0d771c5317b308e045b
fnamg14 <githupsignals@gmail.com>
2026-05-13 11:02:58 +0300

    Update signals.js

--- routes/signals.js
+++ routes/signals.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const signalsController = require('../controllers/signalsController');
 
 router.post('/inr', signalsController.generateINRSignals);
+router.post('/get', signalsController.generateINRSignals);
 router.get('/upcoming', signalsController.getUpcomingSignals);
 router.post('/clear-cache', signalsController.clearCache);
 
